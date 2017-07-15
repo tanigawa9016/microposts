b381022d75e5dcacb7db97629bf601a977076066
kenichiro tanigawa <tanigawa@chitacoh.com>
2017-07-15 11:33:36 +0000

    post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,6 +10,8 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::get('/', 'WelcomeController@index');
+
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
 
@@ -17,10 +19,8 @@ Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
